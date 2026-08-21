0fff48f3ee8b806ba9d1b3c84cf410d98fec871a
Gabriel Sena <gabrielsena10072@2gmail.com>
2026-08-21 04:12:40 -0300

    Corrigindo as falhas geradas pela função diff. Utilizei o merge para filtrar de maneira correta os valores

--- selic.py
+++ selic.py
@@ -1,19 +1,5 @@
 import pandas as pd
 
-
-data_inicio = '01/03/2025'
-data_fim = '18/08/2026'  
-url_selic = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json&dataInicial={data_inicio}&dataFinal={data_fim}'
-def base_dados_selic(): 
- df_selic = pd.read_json(url_selic)
- print(df_selic)
- df_selic['data'] = pd.to_datetime(df_selic['data'], format='%d/%m/%Y')
- df_selic = df_selic.rename(columns={'data' : 'Data'})
- df_selic_resumo = df_selic[df_selic['valor'].diff() != 0].copy()
-
- return df_selic_resumo
-print(base_dados_selic())
-
 datas_reunião = [
  
     {'Data_Vigencia': '2026-01-29', 'Reuniao': 'R1/2026'},
@@ -25,3 +11,23 @@ datas_reunião = [
     {'Data_Vigencia': '2026-11-05', 'Reuniao': 'R7/2026'},
     {'Data_Vigencia': '2026-12-10', 'Reuniao': 'R8/2026'}
 ]
+
+df_datas_reunião = pd.DataFrame(datas_reunião)
+df_datas_reunião['Data_Vigencia'] = pd.to_datetime(df_datas_reunião['Data_Vigencia'])
+
+data_inicio = '01/03/2025'
+data_fim = '18/08/2026'  
+url_selic = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json&dataInicial={data_inicio}&dataFinal={data_fim}'
+def base_dados_selic(): 
+ df_selic = pd.read_json(url_selic)
+ print(df_selic)
+ df_selic['data'] = pd.to_datetime(df_selic['data'], format='%d/%m/%Y')
+ df_selic = df_selic.rename(columns={'data' : 'Data'})
+
+ return df_selic
+
+df_selic = base_dados_selic()
+
+df_selic_resumo = pd.merge(df_datas_reunião, df_selic, left_on='Data_Vigencia', right_on='Data', how='inner' )
+df_selic_resumo = df_selic_resumo.rename(columns={'valor': 'Selic_Real'})
+print(df_selic_resumo)
